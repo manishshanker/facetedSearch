307cac2738be37bc7c2cd052569e0eb804f4eb3f
Manish Shanker <manishshanker@hotmail.com>
2014-01-21 15:31:54 +0000

    added loading message

--- src/lib/js/app/modules/discover/controls/searchFiltering/visualFiltering/VisualFiltering.view.js
+++ src/lib/js/app/modules/discover/controls/searchFiltering/visualFiltering/VisualFiltering.view.js
@@ -15,11 +15,12 @@
             setTimeout(function () {
                 renderGraph(that, data);
                 showGraph(that);
+                that.$el.removeClass("loading");
             }, 300);
         },
         layoutChange: function () {
             var that = this;
-            that.$el.find(".graph-frame").css({
+            that.$el.find(".graph").css({
                 opacity: 0
             });
             setTimeout(function() {
@@ -33,19 +34,19 @@
     });
 
     function hideGraph(that) {
-        that.$el.find(".graph-frame").animate({
+        that.$el.find(".graph").animate({
             opacity: 0
         }, 200)
     }
 
     function showGraph(that) {
-        that.$el.find(".graph-frame").animate({
+        that.$el.find(".graph").animate({
             opacity: 1
         }, 200)
     }
 
     function renderGraph(that, data) {
-        var container = that.$el[0];
+        var container = that.$el.find(".graph")[0];
         var options = {
             nodes: {
                 fontSize: 11,
@@ -64,6 +65,7 @@
         var that = this;
         var selectItemId = parseInt(that.graph.getSelection());
         if (that.lastSelectedNode === selectItemId) {
+            that.$el.addClass("loading");
             that.messageBus.publish("visual-filtering-filtered", selectItemId);
             that.lastSelectedNode = null;
         }
