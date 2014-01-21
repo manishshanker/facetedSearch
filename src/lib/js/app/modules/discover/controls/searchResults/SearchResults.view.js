092e3a387a568c258db767129042cfec7c807a56
Manish Shanker <manishshanker@hotmail.com>
2014-01-21 13:13:37 +0000

    added show more/less behaviour

--- src/lib/js/app/modules/discover/controls/searchResults/SearchResults.view.js
+++ src/lib/js/app/modules/discover/controls/searchResults/SearchResults.view.js
@@ -9,6 +9,14 @@
             },
             "mouseleave .results-item": function(e, item) {
                 $(item).removeClass("item-hover");
+            },
+            "click .show-more": function(e, item) {
+                $(item).parents(".results-item").eq(0).addClass("show-all");
+                e.preventDefault();
+            },
+            "click .show-less": function(e, item) {
+                $(item).parents(".results-item").eq(0).removeClass("show-all");
+                e.preventDefault();
             }
         },
         show: function() {
